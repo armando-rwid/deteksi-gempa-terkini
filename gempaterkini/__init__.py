b744edeb3545c804641484155f0c937dfd5b43fc
sys_armando <patrickarmando87@gmail.com>
2021-11-14 22:41:46 +0900

    Terakhir di tanggal waktu

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,20 +1,44 @@
-def ekstraksi_data():
-    hasil = dict()
-    hasil['tanggal'] = '13 November 2021'
-    hasil['waktu'] = '20:24:51 WIB'
-    hasil['magnitudo'] = '5.0 SR'
-    hasil['kedalaman'] = '118 km'
-    hasil['lokasi'] = {
-        'LU': 1.48,
-        'BT': 134.01
-    }
-    hasil['pusat'] = '53 km BaratLaut HALMAHERABARAT-MALUT'
-    hasil['dampak'] = 'tidak berpotensi tsunami'
+import requests
+from bs4 import BeautifulSoup
 
-    return hasil
+
+def ekstraksi_data():
+
+    try:
+        content = requests.get('https://bmkg.go.id')
+    except Exception:
+        return None
+
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+        hasil = soup.find('span', {'class': 'waktu'})
+        hasil = hasil.text.split(',')
+        tanggal = hasil[1]
+        waktu = hasil[0]
+        magnitudo = soup.find('span', {'class': 'ic magnitude'})
+
+
+        hasil = dict()
+        hasil['tanggal'] = tanggal
+        hasil['waktu'] = waktu
+        hasil['magnitudo'] = magnitudo
+        hasil['kedalaman'] = '118 km'
+        hasil['lokasi'] = {
+            'LU': 1.48,
+            'BT': 134.01
+        }
+        hasil['pusat'] = '53 km BaratLaut HALMAHERABARAT-MALUT'
+        hasil['dampak'] = 'tidak berpotensi tsunami'
+
+        return hasil
+    else:
+        return None
 
 
 def tampilkan_data(result):
+    if result is None:
+        print('Tidak bisa menampilkan data terkini')
+        return
     print('Gempa terakhir berdasarkan BMKG')
     print(f"Tanggal : {result['tanggal']}")
     print(f"Waktu : {result['waktu']}")
